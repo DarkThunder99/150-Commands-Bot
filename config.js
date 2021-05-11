1eed39e1a02aaae73daca033298fa69cfda7101c
DarkThunder99 <75782012+DarkThunder99@users.noreply.github.com>
2021-05-11 09:04:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+exports.TOKEN = '';
+
+exports.DBL_API_KEY = '';
+
+exports.PREFIX = '';
+
+exports.GOOGLE_API_KEY = '';
+
+exports.GENIUS_API_KEY = '';
+
+exports.yandex_API = '';
+
+exports.news_API = '';
+
+exports.giphy_API = '';
+
+exports.AME_API = '';
+
+exports.blague_API = '';
