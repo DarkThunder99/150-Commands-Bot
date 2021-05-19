22da170cda87500bb65db9a7ef1954cce5e7652a
DarkThunder99 <75782012+DarkThunder99@users.noreply.github.com>
2021-05-19 12:22:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,13 @@
 exports.TOKEN = '';
 
-exports.DBL_API_KEY = '';
-
-exports.PREFIX = '';
+exports.PREFIX = '.';
 
 exports.GOOGLE_API_KEY = '';
 
 exports.GENIUS_API_KEY = '';
-
-exports.yandex_API = '';
 
 exports.news_API = '';
 
 exports.giphy_API = '';
 
 exports.AME_API = '';
-
-exports.blague_API = '';
